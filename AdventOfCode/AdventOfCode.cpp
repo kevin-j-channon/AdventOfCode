6677df81947b7cc5936239c68f9d60d186d4d416
Kevin Channon <kevin.channon@paconsulting.com>
2021-12-01 14:25:33 +0000

    Adding reading from file (not working)

--- AdventOfCode/AdventOfCode.cpp
+++ AdventOfCode/AdventOfCode.cpp
@@ -5,6 +5,9 @@
 #include <ranges>
 #include <vector>
 #include <cstdint>
+#include <fstream>
+#include <iterator>
+#include <format>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -21,6 +24,15 @@ namespace AdventOfCode
 			auto depth_increase = aoc::DepthAssessor().GetDepthChange(measurements);
 
 			Assert::AreEqual(uint32_t{ 2 }, depth_increase);
+		}
+
+		TEST_METHOD(CountNetIncreaseFromFile)
+		{
+			std::ifstream data_file("Data/Day1_input.txt");
+
+			auto depth_increase = aoc::DepthAssessor().GetDepthChange(std::ranges::subrange(std::istream_iterator<uint32_t>(data_file), std::istream_iterator<uint32_t>()));
+
+			Logger::WriteMessage(std::format("Depth increase = {}", depth_increase).c_str());
 		}
 	};
 }
--- AdventOfCode/Day1.hpp
+++ AdventOfCode/Day1.hpp
@@ -12,7 +12,7 @@ namespace aoc
 		uint32_t GetDepthChange(const Range_T& data)
 		{
 			auto out = uint32_t{0};
-			for (auto v1 = data.cbegin(), v2 = std::next(data.begin()); v2 != data.cend(); ++v1, ++v2) {
+			for (auto v1 = data.begin(), v2 = std::next(data.begin()); v2 != data.end(); ++v1, ++v2) {
 				out += *v2 - *v1;
 			}
 

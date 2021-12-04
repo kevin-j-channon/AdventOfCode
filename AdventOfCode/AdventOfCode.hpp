9c337a278d034a6fb1d324545d7da55a5edcf943
kevin <kevinchannon@gmail.com>
2021-12-04 22:50:26 +0000

    Adding seperator comments

--- AdventOfCode/AdventOfCode.hpp
+++ AdventOfCode/AdventOfCode.hpp
@@ -11,197 +11,227 @@
 #include <istream>
 #include <bitset>
 
+///////////////////////////////////////////////////////////////////////////////
+
 namespace aoc
 {
 
-	template<typename Value_T>
-	struct Vec2d
-	{
-		Value_T x{};
-		Value_T y{};
-	};
+///////////////////////////////////////////////////////////////////////////////
 
-	struct Exception : public std::runtime_error
-	{
-		Exception(const std::string& msg) : std::runtime_error{ msg } {}
-	};
+template<typename Value_T>
+struct Vec2d
+{
+	Value_T x{};
+	Value_T y{};
+};
 
-	using Direction = Vec2d<int>;
+///////////////////////////////////////////////////////////////////////////////
 
-	template<typename Value_T>
-	Vec2d<Value_T> operator+(const Vec2d<Value_T>& v1, const Vec2d<Value_T>& v2)
+struct Exception : public std::runtime_error
+{
+	Exception(const std::string& msg) : std::runtime_error{ msg } {}
+};
+
+///////////////////////////////////////////////////////////////////////////////
+
+using Direction = Vec2d<int>;
+
+///////////////////////////////////////////////////////////////////////////////
+
+template<typename Value_T>
+Vec2d<Value_T> operator+(const Vec2d<Value_T>& v1, const Vec2d<Value_T>& v2)
+{
+	return { v1.x + v2.x, v1.y + v2.y };
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+struct Aiming
+{
+	int x{};
+	int aim{};
+	int depth{};
+
+	Direction to_direction() const
 	{
-		return { v1.x + v2.x, v1.y + v2.y };
+		return { x, depth };
 	}
 
-
-	struct Aiming
+	Aiming operator+(const Direction& d) const
 	{
-		int x{};
-		int aim{};
-		int depth{};
+		const auto new_aim = this->aim + d.y;
+		return {
+			this->x + d.x,
+			new_aim,
+			this->depth + d.x * new_aim
+		};
+	}
+};
 
-		Direction to_direction() const
-		{
-			return { x, depth };
-		}
+///////////////////////////////////////////////////////////////////////////////
 
-		Aiming operator+(const Direction& d) const
-		{
-			const auto new_aim = this->aim + d.y;
-			return {
-				this->x + d.x,
-				new_aim,
-				this->depth + d.x * new_aim
-			};
-		}
-	};
+struct PowerParams
+{
+	static constexpr uint32_t bit_count = 12;
 
-	struct PowerParams
+	using Bits_t = std::array<bool, bit_count>;
+	Bits_t bits;
+
+	uint32_t gamma_rate() const
 	{
-		static constexpr uint32_t bit_count = 12;
+		return std::accumulate(bits.begin(), bits.end(), uint32_t{ 0 }, [](auto curr, auto next) {
+			curr <<= 1;
+			if (next)
+				curr |= 1;
 
-		using Bits_t = std::array<bool, bit_count>;
-		Bits_t bits;
+			return curr;
+			});
+	}
 
-		uint32_t gamma_rate() const
-		{
-			return std::accumulate(bits.begin(), bits.end(), uint32_t{ 0 }, [](auto curr, auto next) {
-				curr <<= 1;
-				if (next)
-					curr |= 1;
-
-				return curr;
-				});
-		}
-
-		uint32_t epsilon_rate() const
-		{
-			return std::accumulate(bits.begin(), bits.end(), uint32_t{ 0 }, [](auto curr, auto next) {
-				curr <<= 1;
-				if (!next)
-					curr |= 1;
-
-				return curr;
-				});
-		}
-	};
-
-	class Submarine
+	uint32_t epsilon_rate() const
 	{
-	public:
+		return std::accumulate(bits.begin(), bits.end(), uint32_t{ 0 }, [](auto curr, auto next) {
+			curr <<= 1;
+			if (!next)
+				curr |= 1;
 
-		template<size_t WINDOW_SIZE, typename Iter_T>
-		uint32_t depth_score(Iter_T begin, Iter_T end)
-		{
-			auto current_idx = size_t{ 0 };
+			return curr;
+			});
+	}
+};
 
-			auto window = std::array<typename Iter_T::value_type, WINDOW_SIZE>{};
-			for (auto it = window.begin(); it < window.end(); ++it, ++current_idx) {
-				*it = *begin++;
-			}
+///////////////////////////////////////////////////////////////////////////////
 
-			auto prev_score = std::accumulate(window.begin(), window.end(), uint32_t{ 0 });
+class Submarine
+{
+public:
 
-			return std::accumulate(begin, end, 0, [&window, &prev_score, &current_idx](auto curr, auto next) {
+	template<size_t WINDOW_SIZE, typename Iter_T>
+	uint32_t depth_score(Iter_T begin, Iter_T end)
+	{
+		auto current_idx = size_t{ 0 };
 
-				auto new_score = uint32_t{ prev_score - window[current_idx % window.size()] + next };
-
-				auto out = new_score > prev_score ? ++curr : curr;
-
-				prev_score = new_score;
-				window[current_idx % window.size()] = next;
-				++current_idx;
-
-				return out;
-				});
+		auto window = std::array<typename Iter_T::value_type, WINDOW_SIZE>{};
+		for (auto it = window.begin(); it < window.end(); ++it, ++current_idx) {
+			*it = *begin++;
 		}
 
-		template<typename Iter_T>
-		Direction net_direction(Iter_T begin, Iter_T end)
-		{
-			return std::accumulate(begin, end, Direction{});
-		}
-		
-		template<typename Iter_T>
-		Direction net_aiming(Iter_T begin, Iter_T end)
-		{
-			return std::accumulate<>(begin, end, Aiming{}).to_direction();
-		}
+		auto prev_score = std::accumulate(window.begin(), window.end(), uint32_t{ 0 });
 
-		template<typename Iter_T>
-		PowerParams evaluate_power_params(Iter_T begin, Iter_T end)
-		{
-			auto bit_counts = std::accumulate(begin, end, std::array<int, aoc::PowerParams::bit_count>{},
-				[](auto&& curr, auto&& pp) {
-					std::transform(curr.begin(), curr.end(), pp.bits.begin(), curr.begin(),
-						[](auto count, auto bit) {
-							return count + (bit ? 1 : -1);
-						});
+		return std::accumulate(begin, end, 0, [&window, &prev_score, &current_idx](auto curr, auto next) {
 
-					return curr;
-				});
+			auto new_score = uint32_t{ prev_score - window[current_idx % window.size()] + next };
 
-			auto out = PowerParams{};
-			std::transform(bit_counts.begin(), bit_counts.end(), out.bits.begin(), [](auto count) {
-				return count >= 0 ? true : false;
-				});
+			auto out = new_score > prev_score ? ++curr : curr;
+
+			prev_score = new_score;
+			window[current_idx % window.size()] = next;
+			++current_idx;
 
 			return out;
-		}
-	};
-}
+			});
+	}
+
+	template<typename Iter_T>
+	Direction net_direction(Iter_T begin, Iter_T end)
+	{
+		return std::accumulate(begin, end, Direction{});
+	}
+
+	template<typename Iter_T>
+	Direction net_aiming(Iter_T begin, Iter_T end)
+	{
+		return std::accumulate<>(begin, end, Aiming{}).to_direction();
+	}
+
+	template<typename Iter_T>
+	PowerParams evaluate_power_params(Iter_T begin, Iter_T end)
+	{
+		auto bit_counts = std::accumulate(begin, end, std::array<int, aoc::PowerParams::bit_count>{},
+			[](auto&& curr, auto&& pp) {
+				std::transform(curr.begin(), curr.end(), pp.bits.begin(), curr.begin(),
+					[](auto count, auto bit) {
+						return count + (bit ? 1 : -1);
+					});
+
+				return curr;
+			});
+
+		auto out = PowerParams{};
+		std::transform(bit_counts.begin(), bit_counts.end(), out.bits.begin(), [](auto count) {
+			return count >= 0 ? true : false;
+			});
+
+		return out;
+	}
+};
+
+///////////////////////////////////////////////////////////////////////////////
+
+}	// namespace: aoc
+
+///////////////////////////////////////////////////////////////////////////////
 
 namespace std
 {
-	istream& operator>>(istream& is, aoc::Direction& direction)
-	{
-		auto cmd = std::string{};
 
-		direction = aoc::Direction{ 0, 0 };
+///////////////////////////////////////////////////////////////////////////////
 
-		is >> cmd;
+istream& operator>>(istream& is, aoc::Direction& direction)
+{
+	auto cmd = std::string{};
 
-		if (cmd.empty())
-			return is;
+	direction = aoc::Direction{ 0, 0 };
 
-		if (cmd == "forward")
-		{
-			is >> direction.x;
-		}
-		else if (cmd == "up")
-		{
-			is >> direction.y;
-			direction.y *= -1;
-		}
-		else if (cmd == "down")
-		{
-			is >> direction.y;
-		}
-		else {
-			throw aoc::Exception(std::format("Invalid direction string"));
-		}
+	is >> cmd;
 
-		if (is.fail())
-			throw aoc::Exception(std::format("Invalid direction string"));
-
+	if (cmd.empty())
 		return is;
+
+	if (cmd == "forward")
+	{
+		is >> direction.x;
+	}
+	else if (cmd == "up")
+	{
+		is >> direction.y;
+		direction.y *= -1;
+	}
+	else if (cmd == "down")
+	{
+		is >> direction.y;
+	}
+	else {
+		throw aoc::Exception(std::format("Invalid direction string"));
 	}
 
-	istream& operator>>(istream& is, aoc::PowerParams& pp)
-	{
-		pp = aoc::PowerParams{};
+	if (is.fail())
+		throw aoc::Exception(std::format("Invalid direction string"));
 
-		auto param_str = std::string{};
-		is >> param_str;
-
-		if (param_str.length() != aoc::PowerParams::bit_count) {
-			is.setstate(std::ios::failbit);
-			return is;
-		}
-
-		std::transform(param_str.begin(), param_str.end(), pp.bits.begin(), [](auto c) { return c == '1' ? true : false; });
-
-		return is;
-	}
+	return is;
 }
+
+///////////////////////////////////////////////////////////////////////////////
+
+istream& operator>>(istream& is, aoc::PowerParams& pp)
+{
+	pp = aoc::PowerParams{};
+
+	auto param_str = std::string{};
+	is >> param_str;
+
+	if (param_str.length() != aoc::PowerParams::bit_count) {
+		is.setstate(std::ios::failbit);
+		return is;
+	}
+
+	std::transform(param_str.begin(), param_str.end(), pp.bits.begin(), [](auto c) { return c == '1' ? true : false; });
+
+	return is;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+}
+
+///////////////////////////////////////////////////////////////////////////////

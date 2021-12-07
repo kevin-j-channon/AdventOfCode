82e270ec64040a0aef283c369f53bf37be10250d
kevin <kevinchannon@gmail.com>
2021-12-07 20:14:05 +0000

    Adding bingo board deserialization (not complete)

--- AdventOfCode/AdventOfCode.cpp
+++ AdventOfCode/AdventOfCode.cpp
@@ -438,30 +438,9 @@ namespace AdventOfCode
 	{
 	public:
 
-		TEST_METHOD(LoadExampleBingoGame)
+		TEST_METHOD(LoadExampleBingoGameDraws)
 		{
-			constexpr auto game_str =
-				"7, 4, 9, 5\n"
-				"\n"
-				"22 13 17 11  0\n"
-				"8  2 23  4 24\n"
-				"21  9 14 16  7\n"
-				"6 10  3 18  5\n"
-				"1 12 20 15 19\n"
-				"\n"
-				"3 15  0  2 22\n"
-				"9 18 13 17  5\n"
-				"19  8  7 25 23\n"
-				"20 11 10 24  4\n"
-				"14 21 16 12  6\n"
-				"\n"
-				"14 21 17 24  4\n"
-				"10 16 15  9 19\n"
-				"18  8 23 26 20\n"
-				"22 11 13  6  5\n"
-				"2  0 12  3  7";
-
-			std::stringstream ss{ game_str };
+			std::stringstream ss{ "7, 4, 9, 5\n" };
 
 			auto game = aoc::Bingo{};
 
@@ -469,6 +448,23 @@ namespace AdventOfCode
 
 			const auto expected_draws = { 7, 4, 9, 5 };
 			Assert::IsTrue(std::equal(expected_draws.begin(), expected_draws.end(), game.draws().begin()));
+		}
+
+		TEST_METHOD(LoadExampleBingoGameBoards)
+		{
+			constexpr auto board_str =
+				"22 13 17 11  0\n"
+				"8  2 23  4 24\n"
+				"21  9 14 16  7\n"
+				"6 10  3 18  5\n"
+				"1 12 20 15 19\n"
+				"\n";
+
+			std::stringstream ss{ board_str };
+
+			auto board = aoc::Bingo::Board{};
+
+			board.load(ss);
 		}
 	};
 }
--- AdventOfCode/EntertainmentSystems.hpp
+++ AdventOfCode/EntertainmentSystems.hpp
@@ -22,6 +22,38 @@ public:
 
 	using Draws_t = std::vector<uint8_t>;
 
+	class Board
+	{
+		using ValueGrid_t = std::vector<std::vector<uint8_t>>;
+
+	public:
+		Board(uint8_t size)
+			: _size{ size }
+		{}
+
+		void load(std::istream& stream)
+		{
+			for (auto row = 0; row < _size && stream.good(); ++row) {
+				auto line = std::string{};
+				std::getline(stream, line);
+				const auto value_strings = split(line, ',');
+				if (value_strings.size() != _size) {
+					throw Exception("Invalid bingo board size board");
+				}
+
+				auto row_values = ValueGrid_t::value_type(_size, 0);
+				std::transform(value_strings.begin(), value_strings.end(), row_values.begin(), [](auto s) { return static_cast<uint8_t>(std::stol(s)); });
+
+				_numbers.push_back(std::move(row_values));
+			}
+		}
+	private:
+		uint8_t _size;
+		ValueGrid_t _numbers;
+	};
+
+	using Boards_t = std::vector<Board>;
+
 	void load(std::istream& stream)
 	{
 		auto line = std::string{};
@@ -32,9 +64,11 @@ public:
 	}
 
 	const Draws_t& draws() const { return _draws; }
+	const Boards_t& boards() const { return _boards; }
 
 private:
 	Draws_t _draws;
+	Boards_t _boards;
 };
 
 ///////////////////////////////////////////////////////////////////////////////

9659528ba3c966b6fb23ce87b5f5eeb7ade62f3b
kevin <kevinchannon@gmail.com>
2022-01-07 22:00:50 +0000

    Loading insertion rules from stream

--- AdventOfCode/Polymerizer.hpp
+++ AdventOfCode/Polymerizer.hpp
@@ -1,9 +1,19 @@
 #pragma once
 
 #include "Common.hpp"
+#include "StringOperations.hpp"
 
 namespace  aoc
 {
+namespace polymer
+{
+
+using Dimer_t = std::array<char, 2>;
+
+using Monomer_t = char;
+using InsertionRuleTable_t = std::map<Dimer_t, Monomer_t>;
+using InsertionRule_t = InsertionRuleTable_t::value_type;
+
 class Polymer
 {
 public:
@@ -44,4 +54,55 @@ private:
 
 	std::vector<char> _sequence;
 };
+
+class InsertionRuleLoader
+{
+public:
+
+	static InsertionRuleTable_t insertion_rule_table_from_stream(std::istream& is)
+	{
+		auto out = InsertionRuleTable_t{};
+
+		auto line = std::string{};
+		while (!is.eof()) {
+			std::getline(is, line);
+			if (line.empty()) {
+				continue;
+			}
+
+			out.insert(_insertion_rule_from_string(line));
+		}
+
+		return out;
+	}
+
+private:
+
+	static InsertionRule_t _insertion_rule_from_string(const std::string& str)
+	{
+		const auto parts = _split_and_validate_rule_string(str);
+		
+		return { { parts[0][0], parts[0][1] }, parts[2][0] };
+	}
+
+	static std::vector<std::string> _split_and_validate_rule_string(const std::string& str)
+	{
+		const auto parts = split(str, ' ', SplitBehaviour::drop_empty);
+		if (parts.size() != 3) {
+			throw Exception("Inalid insertion rule line");
+		}
+
+		if (parts.front().length() != 2) {
+			throw Exception("Invalid dimer in insertion rule line");
+		}
+
+		if (parts.back().length() != 1) {
+			throw Exception("Invalid monomer in insertion rule line");
+		}
+
+		return parts;
+	}
+};
+
+}
 }
--- AdventOfCode/TestPolymerizer.cpp
+++ AdventOfCode/TestPolymerizer.cpp
@@ -17,9 +17,34 @@ public:
 	{
 		std::stringstream data("NNCB");
 
-		const auto polymer = aoc::Polymer::from_stream(data);
+		const auto polymer = aoc::polymer::Polymer::from_stream(data);
 
 		Assert::AreEqual(size_t{ 4 }, polymer.length());
+	}
+};
+
+TEST_CLASS(TestRuleLoader)
+{
+public:
+	TEST_METHOD(LoadRuleFromStream)
+	{
+		std::stringstream data("AB -> C");
+		const auto rules = aoc::polymer::InsertionRuleLoader::insertion_rule_table_from_stream(data);
+
+		Assert::AreEqual(size_t{ 1 }, rules.size());
+		Assert::AreEqual('C', rules.at(aoc::polymer::Dimer_t{ 'A', 'B' }));
+	}
+
+	TEST_METHOD(LoadMultipleRulesFromStream)
+	{
+		std::stringstream data("AB -> C\nDE -> F\nGH -> I\nJK -> L");
+		const auto rules = aoc::polymer::InsertionRuleLoader::insertion_rule_table_from_stream(data);
+
+		Assert::AreEqual(size_t{ 4 }, rules.size());
+		Assert::AreEqual('C', rules.at(aoc::polymer::Dimer_t{ 'A', 'B' }));
+		Assert::AreEqual('F', rules.at(aoc::polymer::Dimer_t{ 'D', 'E' }));
+		Assert::AreEqual('I', rules.at(aoc::polymer::Dimer_t{ 'G', 'H' }));
+		Assert::AreEqual('L', rules.at(aoc::polymer::Dimer_t{ 'J', 'K' }));
 	}
 };
 }

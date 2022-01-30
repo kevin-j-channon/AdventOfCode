b4024124e60d2dc3815458ff201b106a12e2138f
kevin <kevinchannon@gmail.com>
2022-01-30 09:46:09 +0000

    Creating arbitrarily nested snailfish numbers

--- AdventOfCode/SnailfishNumbers.hpp
+++ AdventOfCode/SnailfishNumbers.hpp
@@ -3,6 +3,8 @@
 #include "StringOperations.hpp"
 #include "Exception.hpp"
 
+#include <any>
+
 namespace aoc
 {
 namespace snailfish
@@ -10,81 +12,136 @@ namespace snailfish
 
 class Value
 {
+
+	using Child = std::variant<uint32_t, std::shared_ptr<Value>>;
+
 public:
+
 	Value()
-		: _left{ 0 }
-		, _right{ 0 }
+		: _children{ uint32_t{0}, uint32_t{0} }
 	{}
 
 	Value(uint32_t left, uint32_t right) 
-		: _left{ left }
-		, _right{ right }
+		: _children{ left, right }
 	{}
 
 	auto operator<=>(const Value&) const = default;
 
-	Value& from_stream(std::istream& is)
+	static std::shared_ptr<Value> from_stream(std::istream& is)
 	{
-		_move_to_start_of_values(is);
-		const auto details = _extract_values_as_string(is);
-		_update_from_comma_sep_value_pair(details);
+		auto line = std::string{};
+		std::getline(is, line);
 
-		return *this;
+		if (line.empty()) {
+			throw IOException("Failed to create snailfish Value - empty line");
+		}
+
+		return create(line.begin(), line.end()).first;
+	}
+
+	template<typename Iter_T>
+	static std::pair<std::shared_ptr<Value>, Iter_T> create(Iter_T current, Iter_T end)
+	{
+		if (*current != '[') {
+			throw IOException("Failed to creaate snailfish Value - invalid openning");
+		}
+
+		auto out = std::make_unique<Value>();
+
+		std::advance(current, 1);
+		std::tie(out->_children.first, current) = _read_value_or_digits(std::move(current), end);
+
+		if (current == end || *current != ',') {
+			throw IOException("Failed to create snailfish value - Unexpected non-numeric value");
+		}
+
+		std::advance(current, 1);
+		std::tie(out->_children.second, current) = _read_value_or_digits(std::move(current), end);
+
+		if (current == end || *current != ']') {
+			throw IOException("Failed to create snailfish value - Unexpected value termination");
+		}
+
+		// Consume the closing bracket at the end of the number.
+		std::advance(current, 1);
+
+		return { std::move(out), std::move(current) };
 	}
 
 	std::wstring as_wstring() const
 	{
-		return std::format(L"[{},{}]", _left, _right);
+		return std::format(L"[{},{}]", _child_to_wstring(_children.first), _child_to_wstring(_children.second));
+	}
+
+	std::string as_string() const
+	{
+		return std::format("[{},{}]", _child_to_string(_children.first), _child_to_string(_children.second));
 	}
 
 private:
 
-	static void _move_to_start_of_values(std::istream& is)
+	template<typename Iter_T>
+	static std::pair<Child, Iter_T> _read_value_or_digits(Iter_T current, Iter_T end)
 	{
-		auto openning_bracket = char{};
-		is.read(&openning_bracket, 1);
-		if (is.fail()) {
-			throw IOException("Failed to read snailfish value: Empty stream");
+		if (*current == '[') {
+			const auto [out, current] = Value::create(current, end);
+			return std::pair<Child, Iter_T>( Child(std::move(out)), std::move(current) );
 		}
-
-		if (openning_bracket != '[') {
-			throw IOException("Failed to read snailfish value: No start brakcet");
+		else {
+			const auto [out, current] = _read_digits(current, end);
+			return std::pair<Child, Iter_T>( Child(std::move(out)), std::move(current) );
 		}
 	}
 
-	static std::string _extract_values_as_string(std::istream& is)
+	template<typename Iter_T>
+	static std::pair<uint32_t, Iter_T> _read_digits(Iter_T current, Iter_T end)
 	{
-		std::stringstream details;
-		const auto it = std::find_if(std::istreambuf_iterator<char>(is), std::istreambuf_iterator<char>(),
-			[&details](auto c) -> bool {
-				if (c != ']') {
-					details << c;
-					return false;
-				}
-
-				return true;
-			});
-
-		if (it == std::istreambuf_iterator<char>()) {
-			throw IOException("Failed to read snailfish value from stream: No end bracket");
+		std::stringstream digits;
+		for (; current != end && std::isdigit(*current); ++current) {
+			digits << *current;
 		}
 
-		return details.str();
+		return { string_to<uint32_t>(digits.str()), current };
 	}
 
-	void _update_from_comma_sep_value_pair(const std::string& str)
+	static bool _is_valid_value_terminator(char c)
 	{
-		const auto parts = split(str, ',');
-		if (parts.size() != 2) {
-			throw IOException("Failed to read snailfish value from stream: invalid number of elements");
+		switch (c) {
+		case ',':
+			return true;
+		default:;
 		}
 
-		_left = string_to<uint32_t>(parts[0]);
-		_right = string_to<uint32_t>(parts[1]);
+		return false;
 	}
 
-	uint32_t _left;
-	uint32_t _right;
+	static std::wstring _child_to_wstring(const Child& child)
+	{
+		return std::visit([](auto&& arg) -> std::wstring {
+			using Arg_t = std::decay_t<decltype(arg)>;
+			if constexpr (std::is_same_v<Arg_t, uint32_t>) {
+				return std::to_wstring(arg);
+			}
+			else if constexpr (std::is_same_v<Arg_t, std::shared_ptr<Value>>) {
+				return arg->as_wstring();
+			}
+			}, child);
+	}
+
+	static std::string _child_to_string(const Child& child)
+	{
+		return std::visit([](auto&& arg) -> std::string {
+			using Arg_t = std::decay_t<decltype(arg)>;
+			if constexpr (std::is_same_v<Arg_t, uint32_t>) {
+				return std::to_string(arg);
+			}
+			else if constexpr (std::is_same_v<Arg_t, std::shared_ptr<Value>>) {
+				return arg->as_string();
+			}
+			}, child);
+	}
+
+	std::pair<Child, Child> _children;
 };
 
 }
--- AdventOfCode/TestSnailfishNumbers.cpp
+++ AdventOfCode/TestSnailfishNumbers.cpp
@@ -23,40 +23,99 @@ public:
 	TEST_METHOD(FromStreamSucceedsForSimplestGoodStream)
 	{
 		std::stringstream data{ "[1,2]" };
-		const auto n1 = aoc::snailfish::Value{}.from_stream(data);
+		const auto n1 = aoc::snailfish::Value::from_stream(data);
 		const auto n2 = aoc::snailfish::Value{ 1, 2 };
 
-		Assert::AreEqual(n2, n1);
+		Assert::AreEqual(n2, *n1);
 	}
 
 	TEST_METHOD(FailsIfStreamIsEmpty)
 	{
 		std::stringstream data{ "" };
-		Assert::ExpectException<aoc::IOException>([&data]() {aoc::snailfish::Value{}.from_stream(data); });
+		Assert::ExpectException<aoc::IOException>([&data]() {aoc::snailfish::Value::from_stream(data); });
 	}
 
 	TEST_METHOD(FailsIfStreamDoesntStartWithSquareBracket)
 	{
 		std::stringstream data{ "{1,2]" };
-		Assert::ExpectException<aoc::IOException>([&data]() {aoc::snailfish::Value{}.from_stream(data); });
+		Assert::ExpectException<aoc::IOException>([&data]() {aoc::snailfish::Value::from_stream(data); });
 	}
 
 	TEST_METHOD(FailsIfStreamDoesntEndWithSquareBracket)
 	{
 		std::stringstream data{ "[1,2" };
-		Assert::ExpectException<aoc::IOException>([&data]() {aoc::snailfish::Value{}.from_stream(data); });
+		Assert::ExpectException<aoc::IOException>([&data]() {aoc::snailfish::Value::from_stream(data); });
 	}
 
 	TEST_METHOD(FailsIfThereAreNotTwoValues)
 	{
 		{
 			std::stringstream data{ "[1]" };
-			Assert::ExpectException<aoc::IOException>([&data]() {aoc::snailfish::Value{}.from_stream(data); });
+			Assert::ExpectException<aoc::IOException>([&data]() {aoc::snailfish::Value::from_stream(data); });
 		}
 
 		{
 			std::stringstream data{ "[1,2,3]" };
-			Assert::ExpectException<aoc::IOException>([&data]() {aoc::snailfish::Value{}.from_stream(data); });
+			Assert::ExpectException<aoc::IOException>([&data]() {aoc::snailfish::Value::from_stream(data); });
+		}
+	}
+
+	TEST_METHOD(FromStreamSucceedsForNestedValue_1)
+	{
+		const auto data_str = "[1,[2,3]]"s;
+
+		std::stringstream data{ data_str };
+		const auto n = aoc::snailfish::Value::from_stream(data);
+
+		Assert::AreEqual(data_str, n->as_string());
+	}
+
+	TEST_METHOD(FromStreamSucceedsForNestedValue_2)
+	{
+		const auto data_str = "[[1,2],3]"s;
+
+		std::stringstream data{ data_str };
+		const auto n = aoc::snailfish::Value::from_stream(data);
+
+		Assert::AreEqual(data_str, n->as_string());
+	}
+
+	TEST_METHOD(FromStreamSucceedsForNestedValue_3)
+	{
+		const auto data_str = "[[1,2],[3,4]]"s;
+
+		std::stringstream data{ data_str };
+		const auto n = aoc::snailfish::Value::from_stream(data);
+
+		Assert::AreEqual(data_str, n->as_string());
+	}
+
+	TEST_METHOD(FromStreamSucceedsForNestedValue_4)
+	{
+		const auto data_str = "[[1,[2,5]],[3,4]]"s;
+
+		std::stringstream data{ data_str };
+		const auto n = aoc::snailfish::Value::from_stream(data);
+
+		Assert::AreEqual(data_str, n->as_string());
+	}
+
+	TEST_METHOD(SampleValues)
+	{
+		const auto data_strs = std::array{
+			"[[[[[9,8],1],2],3],4]"s,
+			"[7,[6,[5,[4,[3,2]]]]]"s,
+			"[[6,[5,[4,[3,2]]]],1]"s,
+			"[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]"s,
+			"[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]"s,
+			"[[[[1,1],[2,2]],[3,3]],[4,4]]"s
+		};
+
+		for (const auto& data_str : data_strs) {
+			std::stringstream data{ data_str };
+			const auto n = aoc::snailfish::Value::from_stream(data);
+
+			Assert::AreEqual(data_str, n->as_string());
 		}
 	}
 };

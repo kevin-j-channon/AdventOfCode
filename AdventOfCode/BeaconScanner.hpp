da8cc059e0cc338188f8e3fd320040e1bcff063b
kevin <kevinchannon@gmail.com>
2022-02-18 19:47:25 +0000

    Day 19: Reading a complete scanner in from a stream

--- AdventOfCode/BeaconScanner.hpp
+++ AdventOfCode/BeaconScanner.hpp
@@ -6,13 +6,27 @@ namespace aoc
 {
 namespace navigation
 {
+using Position_t = Point3D<int>;
+
+class Beacon
+{
+public:
+	Beacon(Position_t position)
+		: _position{ position }
+	{}
+
+	const Position_t& position() const { return _position; }
+
+private:
+	Position_t _position;
+};
 
 class Scanner
 {
-	using Position_t = Point3D<int>;
 public:
 
 	using Id_t = uint32_t;
+	using Beacons_t = std::vector<Beacon>;
 
 	Scanner(Id_t id)
 		: _id{ id }
@@ -21,11 +35,14 @@ public:
 	static Scanner from_stream(std::istream& is)
 	{
 		auto out = Scanner{_id_from_stream(is)};
+		
+		out._beacons = _read_beacons(is);
 
 		return out;
 	}
 
 	const Id_t& id() const { return _id; }
+	const Beacons_t& beacons() const { return _beacons; }
 
 private:
 
@@ -47,8 +64,32 @@ private:
 		return string_to<Id_t>(parts[2]);
 	}
 
+	static Beacons_t _read_beacons(std::istream& is)
+	{
+		auto out = Beacons_t{};
+
+		auto line = std::string{};
+		while (std::getline(is, line)) {
+			if (line.empty()) {
+				return out;
+			}
+
+			const auto parts = split(line, ',');
+			const auto x = string_to<Position_t::XValue_t>(parts[0]);
+			const auto y = string_to<Position_t::YValue_t>(parts[1]);
+			auto z = Position_t::ZValue_t{ 0 };
+			if (parts.size() == 3) {
+				z = string_to<Position_t::ZValue_t>(parts[2]);
+			}
+
+			out.emplace_back(Position_t{ x, y, z });
+		}
+
+		return out;
+	}
+
 	Id_t _id;
-	std::vector<Position_t> _beacon_positions;
+	Beacons_t _beacons;
 };
 }
 }
--- AdventOfCode/TestBeaconScanner.cpp
+++ AdventOfCode/TestBeaconScanner.cpp
@@ -6,13 +6,13 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 namespace test_beacon_scanner
 {
 
+using namespace aoc::navigation;
+
 TEST_CLASS(InitialiseScannerReport)
 {
 public:
-	TEST_METHOD(InitialiseFromStream)
+	TEST_METHOD(InitialiseIdFromStream)
 	{
-		using namespace aoc::navigation;
-
 		constexpr auto data_str =
 			"--- scanner 0 ---\n"
 			"0,2\n"
@@ -26,8 +26,6 @@ public:
 
 	TEST_METHOD(MalformedIdLineRaisesException)
 	{
-		using namespace aoc::navigation;
-
 		constexpr auto data_str =
 			"--- scanner ---\n"
 			"0,2\n"
@@ -36,6 +34,40 @@ public:
 		std::stringstream data{ data_str };
 
 		Assert::ExpectException<aoc::IOException>([&]() { Scanner::from_stream(data); });
+	}
+
+	TEST_METHOD(InitialiseBeaconPositionsFromStream)
+	{
+		constexpr auto data_str =
+			"--- scanner 0 ---\n"
+			"0,2\n"
+			"4,1\n"
+			"3,3\n";
+		std::stringstream data{ data_str };
+
+		const auto scanner = Scanner::from_stream(data);
+		Assert::AreEqual(size_t{ 3 }, scanner.beacons().size());
+
+		{
+			const auto& b = scanner.beacons().at(0);
+			Assert::AreEqual(0, b.position().x);
+			Assert::AreEqual(2, b.position().y);
+			Assert::AreEqual(0, b.position().z);
+		}
+
+		{
+			const auto& b = scanner.beacons().at(1);
+			Assert::AreEqual(4, b.position().x);
+			Assert::AreEqual(1, b.position().y);
+			Assert::AreEqual(0, b.position().z);
+		}
+
+		{
+			const auto& b = scanner.beacons().at(2);
+			Assert::AreEqual(3, b.position().x);
+			Assert::AreEqual(3, b.position().y);
+			Assert::AreEqual(0, b.position().z);
+		}
 	}
 };
 }
